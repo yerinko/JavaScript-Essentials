1c7d604f21c20550410bc0ef8c29a1768fa71f37
yerinko <yerin_o3o@naver.com>
2021-09-10 23:27:09 +0900

    데이터 타입 확인

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-import _ from 'lodash';
+console.log(typeof 'Hello world')
+console.log(typeof 123)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof {})
+console.log(typeof [])
 
-console.log('hello world');
-console.log(_.camelCase('hello world'));
+function getType(data) {
+    return Object.prototype.toString.call(data)
+}
+
+console.log(getType(123))
+console.log(getType(false))
