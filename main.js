06af7ab9c4d1d1bb6a13b9df6ac3c229071fa1b0
yerinko <yerin_o3o@naver.com>
2021-09-13 23:49:48 +0900

    데이터 타입 확인

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import getType from "./getType";
 console.log(typeof 'Hello world')
 console.log(typeof 123)
 console.log(typeof true)
@@ -6,9 +7,7 @@ console.log(typeof null)
 console.log(typeof {})
 console.log(typeof [])
 
-function getType(data) {
-    return Object.prototype.toString.call(data)
-}
+
 
 console.log(getType(123))
 console.log(getType(false))
