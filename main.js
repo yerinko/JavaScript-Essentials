d117465f1e4429f3f8e89ee48980d0bc3db954d9
yerinko <yerin_o3o@naver.com>
2021-09-29 23:57:46 +0900

    조건문 Switch

--- main.js
+++ main.js
@@ -6,6 +6,18 @@ console.log(random())
 
 const a = random()
 
+switch ( a ) {
+    case 0:
+        console.log('a is 0')
+        break
+    case 2:
+        console.log('a is 2')
+        break
+    case 4:
+        console.log('a is 4')
+        break
+}
+
 if ( a === 0 ) {
     console.log('a is 0')
 } else if ( a === 2) {
