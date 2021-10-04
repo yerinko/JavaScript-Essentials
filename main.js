2e1fd436ddfc0a5d0b3a9d1e83eba1cf09ccd5c8
yerinko <yerin_o3o@naver.com>
2021-10-04 22:58:56 +0900

    변수 유효범위

--- main.js
+++ main.js
@@ -40,3 +40,14 @@ for (let i = 0; i < 3; i += 1) {
         })
     }
 }
+
+// 변수 유효범위 (Variable Scope)
+// var, let, const
+
+function scope() {
+    if (true) {
+        const a = 123
+        console.log(a )
+    }
+}
+scope()
