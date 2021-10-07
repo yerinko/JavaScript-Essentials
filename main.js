65541cc3545b96dae92598ee903fbe8abb6ff6b4
yerinko <yerin_o3o@naver.com>
2021-10-08 00:09:28 +0900

    즉시실행함수 IIFE

--- main.js
+++ main.js
@@ -78,3 +78,20 @@ console.log('double: ', double(7))
 
 const doubleArrow = x => x * 2
 console.log('doubleArrow', doubleArrow(7))
+
+// 즉시실행함수
+// IIFE, Immediately-Invoked Function Expression
+
+const a = 7
+function double() {
+    console.log( a * 2)
+}
+double();
+
+(function () {
+    console.log(a*2)
+})();
+
+(function () {
+    console.log(a*2)
+}());
