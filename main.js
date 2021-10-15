9b9488b5f1558c089fcf9d1f08e0cf859eeb8583
yerinko <yerin_o3o@naver.com>
2021-10-15 23:31:27 +0900

    타이머 함수

--- main.js
+++ main.js
@@ -108,3 +108,27 @@ double()
 function double() {
     console.log(a * 2)
 }
+
+// 타이머 함수
+// setTimeout(함수, 시간): 일정 시간 후 함수 실행
+// setInterval(함수, 시간): 시간 간격마다 함수 실행
+// clearTimeout(): 설정된 Timeout 함수를 종료
+// clearInterval(): 설정된 Interval 함수를 종료
+
+const timer = setTimeout(function(){
+    console.log('Yerinko!')
+}, 3000)
+
+const h1El = document.querySelector('h1')
+h1El.addEventListener('click', () => {
+    clearTimeout(timer)
+})
+
+const setTimer = setInterval(function() {
+    console.log('Yerinko!')
+}, 3000)
+
+const h1El2 = document.querySelector('h1')
+h1El2.addEventListener('click', () => {
+    clearInterval(setTimer)
+})
